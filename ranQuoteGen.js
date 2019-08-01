de0daeeafe580fa845c8e7d774eade022d1a4f6a
ColetteMcGrath <53576367+ColetteMcGrath@users.noreply.github.com>
2019-08-01 10:36:49 -0700

    Create ranQuoteGen.js

---
+++ ranQuoteGen.js
@@ -0,0 +1,21 @@
+// Build an object array of quotes and authors
+// additional info is optional: cit, year
+
+// set up the array as follows (fill in the blanks)
+
+var nameHere = [
+  {
+    quote: 'quote here',
+    author: 'author here',
+    citiation: 'optional',
+    year: 'optional'
+  },
+  {
+    repeat above code
+  },
+];
+
+// create a function that pulls one of the quotes and info out of the array at random
+
+
+// create a function that prints the random quote to the screen
